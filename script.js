e547889d153cb2a6331c371e94c26dc0fa3a4f96
wxkvito <44097952+wxkvito@users.noreply.github.com>
2019-11-28 12:39:33 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,148 +1,187 @@
-const colums_size = 11;
-const rows_size = 11;
-const field_size = 50;
-const padding = 10;
+const Colum_Size = 75 // width
+const Rows_Size = 50 // height
+const Field_Size = 5 // scale  
+const PADDING = 10 // padding of field
+const TRACTORS_NUMBER = 1000 //each more tractor mean more draw speed
 
-const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
-const map = createMap();
-const tracktor = { x: 0, y: 0 }
-setField(0, 0, 'space');
+const canvas = document.querySelector('canvas') 
+const context = canvas.getContext('2d')
+const map = createMap()
 
-init();
-start();
+const tractors = []
+setField(0, 0, 'space')
 
-function start() {
-    requestAnimationFrame(tick)
+init()
+start()
+
+function start () {
+	requestAnimationFrame(tick)
 }
 
-function tick(timestamp) {
-    moveTracktor()
+function tick (timestamp) { //base function for use other functions
+	moveTractors()
 
-    clearCanvas()
-    drawMap()
-    drawTracktor()
+	clearCanvas()
+	drawMap()
 
-    requestAnimationFrame(tick)
+	if (!isMaze()) {
+		drawTractors()
+		requestAnimationFrame(tick)
+	}
 }
 
-function init() {
-    canvas.width = padding * 2 + colums_size * field_size
-    canvas.height = padding * 2 + rows_size * field_size
-}
-function drawMap() {
-    for (let x = 0; x < colums_size; x++) {
-        for (let y = 0; y < rows_size; y++) {
-            if (getField(x, y) === 'wall') {
-                context.fillStyle = 'black'
-                context.beginPath()
-                context.rect(padding + x * field_size, padding + y * field_size, field_size, field_size)
-                context.fill()
-            }
-        }
-    }
+function init () {
+	canvas.width = PADDING * 2 + Colum_Size * Field_Size
+	canvas.height = PADDING * 2 + Rows_Size * Field_Size
 
-}
-function drawTracktor() {
-    context.fillStyle = 'red'
-    context.beginPath()
-    context.rect(padding + tracktor.x * field_size, padding + tracktor.y * field_size, field_size, field_size)
-    context.fill()
+	for (let i = 0; i < TRACTORS_NUMBER; i++) {
+		tractors.push({ x: 0, y: 0 })
+	}
 }
 
-function moveTracktor() {
-    const directs = []
-    if (tracktor.x > 0) {
-        directs.push('left')
-    }
-    if (tracktor.x < colums_size - 2) {
-        directs.push('right')
-    }
-    if (tracktor.y > 0) {
-        directs.push('up')
-    }
-    if (tracktor.y < rows_size - 2) {
-        directs.push('down')
-    }
-    const direct = getRandomFrom(directs)
-
-    switch (direct) {
-        case 'left':
-            if (getField(tracktor.x - 2, tracktor.y) === 'wall') {
-                setField(tracktor.x - 1, tracktor.y, 'space')
-                setField(tracktor.x - 2, tracktor.y, 'space')
-            }
-            tracktor.x -= 2 // tracktor.x = tracktor.x -2
-            break
-        case 'right':
-            if (getField(tracktor.x + 2, tracktor.y) === 'wall') {
-                setField(tracktor.x + 1, tracktor.y, 'space')
-                setField(tracktor.x + 2, tracktor.y, 'space')
-            }
-            tracktor.x += 2 // tracktor.x = tracktor.x +2
-            break
-        case 'up':
-            if (getField(tracktor.x, tracktor.y - 2) === 'wall') {
-                setField(tracktor.x, tracktor.y - 1, 'space')
-                setField(tracktor.x, tracktor.y - 2, 'space')
-            }
-            tracktor.y -= 2 // tracktor.x = tracktor.y -2
-            break
-        case 'down':
-            if (getField(tracktor.x, tracktor.y + 2) === 'wall') {
-                setField(tracktor.x, tracktor.y + 1, 'space')
-                setField(tracktor.x, tracktor.y + 2, 'space')
-            }
-            tracktor.y += 2 // tracktor.x = tracktor.y +2
-            break
-    }
+function drawMap () {
+	for (let x = 0; x < Colum_Size; x++) {
+		for (let y = 0; y < Rows_Size; y++) {
+			if (getField(x, y) === 'wall') {
+				context.fillStyle = 'black'
+				context.beginPath()
+				context.rect(PADDING + x * Field_Size, PADDING + y * Field_Size, Field_Size, Field_Size)
+				context.fill()
+			}
+		}
+	}
 }
 
-function clearCanvas() {
-    context.fillStyle = 'black'
-    context.beginPath()
-    context.rect(0, 0, canvas.width, canvas.height)
-    context.fill()
-
-    context.fillStyle = 'white'
-    context.beginPath()
-    context.rect(padding, padding, canvas.width - padding * 2, canvas.height - padding * 2)
-    context.fill()
-
+function drawTractors () {
+	for (const tractor of tractors) {
+		context.fillStyle = 'red'
+		context.beginPath()
+		context.rect(PADDING + tractor.x * Field_Size, PADDING + tractor.y * Field_Size, Field_Size, Field_Size)
+		context.fill()		
+	}
 }
 
-function createMap() {
-    const map = []
-    for (let y = 0; y < rows_size; y++) {
-        const row = []
+function moveTractors () {
+	for (const tractor of tractors) {
 
-        for (let x = 0; x < colums_size; x++) {
-            row.push('wall')
-        }
-        map.push(row)
-    }
-    return map
+		const directs = []
 
+		if (tractor.x > 0) {
+			directs.push('left')
+		}
+
+		if (tractor.x < Colum_Size - 2) {
+			directs.push('right')
+		}
+
+		if (tractor.y > 0) {
+			directs.push('up')
+		}
+
+		if (tractor.y < Rows_Size - 2) {
+			directs.push('down')
+		}
+
+		const direct = getRandomFrom(directs)
+
+		switch (direct) {
+			case 'left':
+				if (getField(tractor.x - 2, tractor.y) === 'wall') {
+					setField(tractor.x - 1, tractor.y, 'space')
+					setField(tractor.x - 2, tractor.y, 'space')
+				}
+
+				tractor.x -= 2 // tractor.x = tractor.x - 2
+				break
+			case 'right':
+				if (getField(tractor.x + 2, tractor.y) === 'wall') {
+					setField(tractor.x + 1, tractor.y, 'space')
+					setField(tractor.x + 2, tractor.y, 'space')
+				}
+
+				tractor.x += 2 // tractor.x = tractor.x + 2
+				break
+			case 'up':
+				if (getField(tractor.x, tractor.y - 2) === 'wall') {
+					setField(tractor.x, tractor.y - 1, 'space')
+					setField(tractor.x, tractor.y - 2, 'space')
+				}
+
+				tractor.y -= 2 // tractor.y = tractor.y - 2
+				break
+			case 'down':
+				if (getField(tractor.x, tractor.y + 2) === 'wall') {
+					setField(tractor.x, tractor.y + 1, 'space')
+					setField(tractor.x, tractor.y + 2, 'space')
+				}
+
+				tractor.y += 2 // tractor.y = tractor.y + 2
+				break
+		}
+	}
 }
 
-function getField(x, y) {
-    if (x < 0 || x >= colums_size || y < 0 || y >= rows_size) {
-        return null
+function clearCanvas () {
+	context.fillStyle = 'black'
+	context.beginPath()
+	context.rect(0, 0, canvas.width, canvas.height)
+	context.fill()
 
-    }
-    return map[y][x]
+	context.fillStyle = 'white'
+	context.beginPath()
+	context.rect(PADDING, PADDING, canvas.width - PADDING * 2, canvas.height - PADDING * 2)
+	context.fill()
 }
 
+function createMap () {
+	const map = []
 
-function setField(x, y, value) {
-    if (x < 0 || x >= colums_size || y < 0 || y >= rows_size) {
-        return null
+	for (let y = 0; y < Rows_Size; y++) {
+		const row = []
 
-    }
-    map[y][x] = value
+		for (let x = 0; x < Colum_Size; x++) {
+			row.push('wall')
+		}
+
+		map.push(row)
+	}
+
+	return map
 }
 
-function getRandomFrom(array) {
-    const index = Math.floor(Math.random() * array.lenght)
-    return array[index]
+function getField (x, y) {
+	if (x < 0 || x >= Colum_Size || y < 0 || y >= Rows_Size) {
+		return null
+	}
+
+	return map[y][x]
+}
+
+function setField (x, y, value) {
+	if (x < 0 || x >= Colum_Size || y < 0 || y >= Rows_Size) {
+		return null
+	}
+
+	map[y][x] = value
+}
+
+function getRandomFrom (array) {
+	const index = Math.floor(Math.random() * array.length)
+	return array[index]
+}
+
+function isEven (n) {
+	return n % 2 === 0
+}
+
+function isMaze () {
+	for (let x = 0; x < Colum_Size; x++) {
+		for (let y = 0; y < Rows_Size; y++) {
+			if (isEven(x) && isEven(y) && getField(x, y) === 'wall') {
+				return false
+			}
+		}
+	}
+
+	return true
 }
